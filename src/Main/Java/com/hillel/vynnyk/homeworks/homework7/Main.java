8d9b78e55e2ee6fb53d89753c5e384d8c27c4284
Vynnyk Vita <vitavynnyk1995@gmail.com>
2022-12-04 17:25:28 +0200

    Counter Strike new

---
+++ src/Main/Java/com/hillel/vynnyk/homeworks/homework7/Main.java
@@ -0,0 +1,14 @@
+package com.hillel.vynnyk.homeworks.homework7;
+
+public class Main {
+    public static void main(String[] args) {
+        int counter = 0 ;
+        for (int i = 1; i <=100 ; i++) {
+            if (!(String.valueOf(i).contains("4") || String.valueOf(i).contains("9"))){
+                System.out.println("Number of spaceship: " + i);
+                counter++;
+            }
+        }
+        System.out.println("Total number of spaceships: " + counter);
+    }
+}
