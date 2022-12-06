79d4bff1b13f79142c6bdb0ec92ffd42bef2d7c2
Vynnyk Vita <vitavynnyk1995@gmail.com>
2022-12-06 20:15:40 +0200

    Play

---
+++ src/Main/Java/com/hillel/vynnyk/homeworks/homework8/Main.java
@@ -0,0 +1,41 @@
+package com.hillel.vynnyk.homeworks.homework8;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int random = (int) (Math.random() * 11);
+//        System.out.println(random);
+
+        System.out.println("Please enter your number:");
+        int i = 0;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                if (number == random) {
+                    System.out.println("Congratulations! You win!");
+                    break;
+                }
+                i++;
+                if (i == 3) {
+                    System.out.println("Sorry, you lost(");
+                    break;
+                } else {
+                    System.out.println("Try again!");
+                }
+            } else {
+                System.out.println("Wrong data! Try again!");
+            }
+            scanner.nextLine();
+        }
+
+    }
+}
+
+
+
+
+
+
