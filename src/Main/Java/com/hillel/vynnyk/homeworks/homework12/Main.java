68181b00d0660ad40eeb4c91b250452913734e42
Vynnyk Vita <vitavynnyk1995@gmail.com>
2022-12-25 18:10:49 +0200

    Method personInfo

--- src/Main/Java/com/hillel/vynnyk/homeworks/homework12/Main.java
+++ src/Main/Java/com/hillel/vynnyk/homeworks/homework12/Main.java
@@ -3,17 +3,15 @@ package com.hillel.vynnyk.homeworks.homework12;
 public class Main {
     public static void main(String[] args) {
         Person one = new Person();
+
         one.personInfo("Will ", "Smith ", "New York", "2936729462846");
 
-        Person two = new Person();
-        two.personInfo("Jackie ","Chan ","Shanghai","12312412412");
+        one.personInfo("Jackie ","Chan ","Shanghai","12312412412");
 
-        Person three = new Person();
-        three.personInfo("Sherlock ", "Holmes ","London", "37742123513");
+        one.personInfo("Sherlock ", "Holmes ","London", "37742123513");
 
     }
 }
-
 class Person {
     public String personInfo (String firstName, String secondName, String city, String phoneNumber) {
         String info = "To call to citizen " + firstName + secondName + "from city " + city + ", "
